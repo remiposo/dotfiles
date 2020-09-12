c1712dc3522813e1dc54a8c9bb6fb1bbf343a098
Kiichi Isobe <isobe180354@gmail.com>
2020-09-12 03:19:08 +0000

    Add rbenv_install

---
+++ rbenv_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+~/.rbenv/bin/rbenv init
