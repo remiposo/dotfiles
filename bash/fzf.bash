95ed9d9affe56b48efcccc11b3f60e0da00204f2
remiposo <isobe180354@gmail.com>
2022-02-21 16:11:29 +0000

    Add setting of fzf in pacman

--- bash/fzf.bash
+++
@@ -1,8 +0,0 @@
-frg() {
-  local INITIAL_QUERY=''
-  local RG_PREFIX='rg --column --line-number --no-heading --color=always --smart-case '
-  FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
-    fzf --bind "change:reload:$RG_PREFIX {q}" \
-        --ansi --phony --height=100%\
-        --preview 'bat --color=always "$(echo {} | cut -f 1 --delim ":")"'
-}
