5e7bf122b3ca26bddef60f8d9ac97e8086819326
remiposo <57336808+remiposo@users.noreply.github.com>
2022-12-25 14:29:23 +0000

    Add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = ','
+
+vim.o.showmode = false
+vim.o.number = true
+vim.o.cursorline = true
+vim.o.swapfile = false
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.list = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.signcolumn = 'yes'
+
+vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>')
