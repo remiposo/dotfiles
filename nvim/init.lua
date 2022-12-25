16479d2b4c8a5d6b2d5bbb47271809a6c7644931
remiposo <57336808+remiposo@users.noreply.github.com>
2022-12-25 17:53:01 +0000

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,66 @@
+-- vi: set ft=lua :
+
+-- #----------------------------------------------#
+-- # common variables                             #
+-- #----------------------------------------------#
 vim.g.mapleader = ','
 
-vim.o.showmode = false
-vim.o.number = true
-vim.o.cursorline = true
-vim.o.swapfile = false
+-- #----------------------------------------------#
+-- # common options                               #
+-- #----------------------------------------------#
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.list = true
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.list = true
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-vim.o.signcolumn = 'yes'
-
+-- #----------------------------------------------#
+-- # common keymaps                               #
+-- #----------------------------------------------#
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'gj', 'j')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('n', 'gk', 'k')
+vim.keymap.set('n', 'q', '<Nop>')
+vim.keymap.set('n', '<C-a>', '^')
+vim.keymap.set('n', '<C-e>', '$')
 vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>')
+vim.keymap.set('n', 's', '<Nop>')
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sl', '<C-w>l')
+-- transfer yanked to clipper runnning on localhost:8377
+vim.keymap.set('n', '<leader>y', ':call system("nc -c localhost 8377", @0)<CR>')
+vim.keymap.set('i', '<C-h>', '<Left>')
+vim.keymap.set('i', '<C-j>', '<Down>')
+vim.keymap.set('i', '<C-k>', '<Up>')
+vim.keymap.set('i', '<C-l>', '<Right>')
+vim.keymap.set('c', '<C-p>', '<Up>')
+vim.keymap.set('c', '<C-n>', '<Down>')
+
+-- #----------------------------------------------#
+-- # plugins                                      #
+-- #----------------------------------------------#
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'EdenEast/nightfox.nvim'
+end)
+
+require('nightfox').setup({
+  options = {
+    transparent = true,
+  },
+})
+vim.cmd('colorscheme nordfox')
