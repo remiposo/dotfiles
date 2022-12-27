994796bab933a86f87a300c1f67588df76c2b9c1
remiposo <57336808+remiposo@users.noreply.github.com>
2022-12-27 04:22:15 +0000

    Add nvim-lsp-setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,10 +33,8 @@ vim.keymap.set('n', 'q', '<Nop>')
 vim.keymap.set('n', '<C-a>', '^')
 vim.keymap.set('n', '<C-e>', '$')
 vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>')
-vim.keymap.set('n', 's', '<Nop>')
-vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
-vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('n', 's', '<Nop>')
 vim.keymap.set('n', 'sh', '<C-w>h')
 vim.keymap.set('n', 'sj', '<C-w>j')
 vim.keymap.set('n', 'sk', '<C-w>k')
@@ -56,6 +54,7 @@ vim.keymap.set('c', '<C-n>', '<Down>')
 require('packer').startup(function(use)
   use "EdenEast/nightfox.nvim"
   use 'folke/tokyonight.nvim'
+  use 'neovim/nvim-lspconfig'
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
@@ -65,6 +64,8 @@ require('packer').startup(function(use)
     run = ':TSUpdate',
   }
   use 'wbthomason/packer.nvim'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
 end)
 
 require('nightfox').setup({
@@ -87,9 +88,45 @@ require('nvim-treesitter.configs').setup({
   ensure_installed = {
     'go',
     'lua',
-    'ruby',
   },
   highlight = {
     enable = true,
   },
+})
+
+require('mason').setup()
+require("mason-lspconfig").setup()
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', 'gp', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', 'gn', vim.diagnostic.goto_next, opts)
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
+end
+
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+
+local on_attach_gopls = function(client, bufnr)
+  on_attach(client, bufnr)
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = vim.api.nvim_create_augroup('FormatGopls', { clear = true }),
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' }}, apply = true})
+      vim.lsp.buf.formatting_seq_sync()
+    end
+  })
+end
+
+require('lspconfig')['gopls'].setup({
+    on_attach = on_attach_gopls,
+    flags = lsp_flags,
 })
