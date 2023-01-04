71516e52ea7a6373a973301b3f3a8721d697cfbb
remiposo <57336808+remiposo@users.noreply.github.com>
2023-01-04 04:14:09 +0000

    Setup plugins in startup of packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,8 +44,105 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp-document-symbol'
   use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use 'hrsh7th/cmp-path'
-  use 'hrsh7th/nvim-cmp'
-  use 'neovim/nvim-lspconfig'
+  use {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      local lspkind = require('lspkind')
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert(),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+          { name = 'nvim_lsp_signature_help' },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            menu = ({
+              buffer = '[Buffer]',
+              nvim_lsp = '[LSP]',
+              path = '[Path]',
+            }),
+          }),
+        },
+      })
+
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+          { name = 'nvim_lsp_document_symbol' },
+        },
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' },
+            },
+          },
+        }),
+      })
+    end,
+  }
+  use {
+    'neovim/nvim-lspconfig',
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup()
+
+      local on_attach = function(client, bufnr)
+        if client.server_capabilities.documentSymbolProvider then
+          require('nvim-navic').attach(client, bufnr)
+        end
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
+      end
+
+      local lsp_flags = {
+        debounce_text_changes = 150,
+      }
+
+      local on_attach_gopls = function(client, bufnr)
+        on_attach(client, bufnr)
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = vim.api.nvim_create_augroup('FormatGopls', { clear = true }),
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+            vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' }}, apply = true })
+          end
+        })
+      end
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      require('lspconfig')['gopls'].setup({
+        on_attach = on_attach_gopls,
+        flags = lsp_flags,
+        capabilities = capabilities,
+      })
+      require('lspconfig')['sumneko_lua'].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+        capabilities = capabilities,
+      })
+    end,
+  }
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons' },
@@ -68,6 +165,17 @@ require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
     requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local tele_builtin = require('telescope.builtin')
+      local keymap_opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '<leader>f', tele_builtin.find_files, keymap_opts)
+      vim.keymap.set('n', '<leader>g', tele_builtin.live_grep, keymap_opts)
+      vim.keymap.set('n', '<leader>b', tele_builtin.buffers, keymap_opts)
+      vim.keymap.set('n', '<leader>c', tele_builtin.commands, keymap_opts)
+      require('telescope').load_extension('file_browser')
+      vim.keymap.set('n', '<leader>b', require('telescope').extensions.file_browser.file_browser, keymap_opts)
+      require('telescope').load_extension('noice')
+    end,
   }
   use { 'nvim-telescope/telescope-file-browser.nvim' }
   use {
@@ -104,105 +212,3 @@ require('packer').startup(function(use)
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
 end)
-
-
-require('mason').setup()
-require('mason-lspconfig').setup()
-
-local on_attach = function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    require('nvim-navic').attach(client, bufnr)
-  end
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
-end
-
-local lsp_flags = {
-  debounce_text_changes = 150,
-}
-
-local on_attach_gopls = function(client, bufnr)
-  on_attach(client, bufnr)
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    group = vim.api.nvim_create_augroup('FormatGopls', { clear = true }),
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ async = false })
-      vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' }}, apply = true })
-    end
-  })
-end
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')['gopls'].setup({
-  on_attach = on_attach_gopls,
-  flags = lsp_flags,
-  capabilities = capabilities,
-})
-require('lspconfig')['sumneko_lua'].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' },
-      },
-    },
-  },
-  capabilities = capabilities,
-})
-
-local tele_builtin = require('telescope.builtin')
-local keymap_opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>f', tele_builtin.find_files, keymap_opts)
-vim.keymap.set('n', '<leader>g', tele_builtin.live_grep, keymap_opts)
-vim.keymap.set('n', '<leader>b', tele_builtin.buffers, keymap_opts)
-vim.keymap.set('n', '<leader>c', tele_builtin.commands, keymap_opts)
-require('telescope').load_extension('file_browser')
-vim.keymap.set('n', '<leader>b', require('telescope').extensions.file_browser.file_browser, keymap_opts)
-require('telescope').load_extension('noice')
-
-local cmp = require('cmp')
-local lspkind = require('lspkind')
-cmp.setup({
-  mapping = cmp.mapping.preset.insert(),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'nvim_lsp_signature_help' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',
-      menu = ({
-        buffer = '[Buffer]',
-        nvim_lsp = '[LSP]',
-        path = '[Path]',
-      }),
-    }),
-  },
-})
-
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' },
-    { name = 'nvim_lsp_document_symbol' },
-  },
-})
-
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-    {
-      name = 'cmdline',
-      option = {
-        ignore_cmds = { 'Man', '!' },
-      },
-    },
-  }),
-})
