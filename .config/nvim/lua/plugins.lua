0c74a79caa04a2f808e8be59ba7201b6eb7d4591
remiposo <remiposo@proton.me>
2023-02-13 14:20:35 +0900

    Rename sumneko_lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -158,7 +158,7 @@ require('packer').startup(function(use)
         flags = lsp_flags,
         capabilities = capabilities,
       })
-      require('lspconfig')['sumneko_lua'].setup({
+      require('lspconfig')['lua_ls'].setup({
         on_attach = on_attach,
         flags = lsp_flags,
         settings = {
