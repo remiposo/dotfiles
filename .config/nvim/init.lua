0aac01bd2f8fbbe54527e7ec5a0c60f812979a5d
remiposo <remiposo@proton.me>
2023-02-14 23:55:31 +0900

    Use single quote

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,14 +41,14 @@ vim.keymap.set('n', 'gp', vim.diagnostic.goto_prev, keymap_opts)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require('lazy').setup('plugins')
