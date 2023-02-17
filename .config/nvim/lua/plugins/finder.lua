8497712c7286b6215b56e4899b0caff9e7565461
remiposo <remiposo@proton.me>
2023-02-17 15:53:47 +0900

    Follow symlink in telescope.find_files

--- .config/nvim/lua/plugins/finder.lua
+++ .config/nvim/lua/plugins/finder.lua
@@ -15,7 +15,7 @@ return {
       local keymap_opts = { noremap = true, silent = true }
 
       vim.keymap.set('n', '<leader>f', builtin('find_files', {
-        find_command = { 'fd', '-H', '-t', 'file', '-E', '.git/' },
+        find_command = { 'fd', '-HL', '-tf', '-tl', '-E', '.git/' },
       }), keymap_opts)
       vim.keymap.set('n', '<leader>g', builtin('live_grep'), keymap_opts)
       vim.keymap.set('n', '<leader>b', builtin('buffers'), keymap_opts)
