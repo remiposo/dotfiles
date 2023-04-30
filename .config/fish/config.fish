24c7c083107c498839e818adf022993fc3f6dc01
remiposo <57336808+remiposo@users.noreply.github.com>
2023-04-30 10:53:45 +0900

    Add catppuccin fzf and starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,14 @@ if status is-login
     set -x VOLTA_HOME $HOME/.volta
     set -x PATH $VOLTA_HOME/bin $PATH
   end
+
+  if command -sq fzf
+    set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border' \
+      '--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284' \
+      '--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf' \
+      '--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284'
+    set FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}'"
+  end
 end
 
 if status is-interactive
