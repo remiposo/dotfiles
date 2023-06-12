a96ea84e62aa2dbc13b346ae4c1c096ebf17bb36
remiposo <57336808+remiposo@users.noreply.github.com>
2023-06-12 09:57:58 +0900

    Fix keymap conflict

--- .config/hammerspoon/lib/windows.lua
+++ .config/hammerspoon/lib/windows.lua
@@ -1,24 +1,24 @@
 return function()
-  hs.hotkey.bind('alt', 'h', function()
+  hs.hotkey.bind('alt', 'left', function()
     local win = hs.window.focusedWindow()
     if win and not(win:isFullScreen()) then
       local unitRect = hs.geometry.rect(0, 0, 0.5, 1)
       win:moveToUnit(unitRect)
     end
   end)
-  hs.hotkey.bind('alt', 'j', function()
+  hs.hotkey.bind('alt', 'down', function()
     local win = hs.window.focusedWindow()
     if win and not(win:isFullScreen()) then
       win:minimize()
     end
   end)
-  hs.hotkey.bind('alt', 'k', function()
+  hs.hotkey.bind('alt', 'up', function()
     local win = hs.window.focusedWindow()
     if win and not(win:isFullScreen()) then
       win:maximize()
     end
   end)
-  hs.hotkey.bind('alt', 'l', function()
+  hs.hotkey.bind('alt', 'right', function()
     local win = hs.window.focusedWindow()
     if win and not(win:isFullScreen()) then
       local unitRect = hs.geometry.rect(0.5, 0, 0.5, 1)
