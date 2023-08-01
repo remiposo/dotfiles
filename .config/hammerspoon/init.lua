e1c10de66ba900297625f7de2309f95969456653
remiposo <57336808+remiposo@users.noreply.github.com>
2023-08-01 13:52:47 +0900

    Move to raycast

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,12 +1,7 @@
 local input = require "lib/input"
-local windows = require "lib/windows"
-local app = require "lib/app"
 
 switchInputEvent = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, input.switchWithCmd)
 switchInputEvent:start()
 
 switchWithEsc = hs.eventtap.new({hs.eventtap.event.types.keyDown}, input.switchWithEsc)
 switchWithEsc:start()
-
-windows()
-app()
